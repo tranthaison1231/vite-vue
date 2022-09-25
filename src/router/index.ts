59e01628d347e5681e7e1e6b82702ef158bdf8e3
Son Tran <son.tran@team.enouvo.com>
2022-09-25 23:37:56 +0700

    feat(main): remove forder routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "../pages/HomePage.vue";
+import HomePage from "#/pages/HomePage.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: HomePage,
+  },
+  {
+    path: "/about",
+    component: () => import("../pages/AboutPage.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomePage,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../pages/AboutPage.vue"),
-    },
-  ],
+  routes: routes,
 });
 
 export default router;
