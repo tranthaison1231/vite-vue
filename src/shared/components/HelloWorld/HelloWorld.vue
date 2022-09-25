6007c8308af75477127727bd4ec5dbe2fd8fa0a6
Son Tran <son.tran@team.enouvo.com>
2022-09-25 21:45:44 +0700

    feat(master): add lintstaged

--- src/shared/components/HelloWorld/HelloWorld.vue
+++ src/shared/components/HelloWorld/HelloWorld.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { useCounterStore } from "#/shared/stores/counter";
-
-const { count } = useCounterStore();
 defineProps<{
   msg: string;
 }>();
@@ -12,7 +9,6 @@ defineProps<{
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      <p>{{ count }}</p>
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
       What's next?
